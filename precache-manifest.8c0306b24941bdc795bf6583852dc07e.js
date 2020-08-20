7f0bdf3129efa78d164463798cbee77abc56a596
kuchi <burner.0107@gmail.com>
2020-08-20 13:56:44 -0400

    Updates

---
+++ precache-manifest.8c0306b24941bdc795bf6583852dc07e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28870517eb34b4bab40d5d9a308a2546",
+    "url": "/video-labeller/index.html"
+  },
+  {
+    "revision": "af823c3a0f3c330bc669",
+    "url": "/video-labeller/static/css/main.a3287a3a.chunk.css"
+  },
+  {
+    "revision": "5fdf84ccc863a8b58d34",
+    "url": "/video-labeller/static/js/2.423a626e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/video-labeller/static/js/2.423a626e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af823c3a0f3c330bc669",
+    "url": "/video-labeller/static/js/main.e1f921d9.chunk.js"
+  },
+  {
+    "revision": "e5f58e69f3e0c614bcbb",
+    "url": "/video-labeller/static/js/runtime-main.fcb85703.js"
+  },
+  {
+    "revision": "1d358d7f34bb28a4f3469042db5d262c",
+    "url": "/video-labeller/static/media/corgi_demo.1d358d7f.mp4"
+  },
+  {
+    "revision": "06c48d1147c1d345a5a5d3a9918fe1f7",
+    "url": "/video-labeller/static/media/pomsky_demo.06c48d11.mp4"
+  }
+]);
